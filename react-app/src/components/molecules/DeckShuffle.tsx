10923481fbba37f02057055cc3b5d80df674cee9
Tanahashi Shun <tanahashi@hashup.co.jp>
2023-06-22 20:42:52 +0900

    カードの種類と位置を同時に設定 を追加

--- react-app/src/components/molecules/DeckShuffle.tsx
+++ react-app/src/components/molecules/DeckShuffle.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import deckOrder from '../atoms/DeckOrder'
 import deckPosition from '../atoms/DeckPosition'
-import { Button, Heading } from '@chakra-ui/react'
+import { Box, Button, Heading, Text } from '@chakra-ui/react'
+import cardNumberChanger from '../atoms/CardNumberChanger'
+import tarotInfomation from '../atoms/TarotInfomation'
 
 export type DeckCardType = {
     stackingOrder: number
@@ -9,7 +11,7 @@ export type DeckCardType = {
 }
 export const useDeckShuffle = () => {
     const deckShuffle = async () => {
-        const deckStackingOrder = await deckOrder({ number: 22, min_num: 1, max_num: 22 })
+        const deckStackingOrder = await deckOrder({ number: 22, min_num: 0, max_num: 21 })
         const deckPositionOrder = await deckPosition({ number: 22, min_num: 0, max_num: 1 })
         const numberOfDeck = [...Array(22)].map((_, i) => i)
         const shuffleDeck = numberOfDeck.map((index) => {
@@ -39,6 +41,25 @@ const DeckShuffle = () => {
         <>
             <Heading ml={4}>デバッグ</Heading>
             <Button onClick={() => shuffleDeck()}>シャッフル</Button>
+            {deck.map((card, index) => {
+                const tarotCard = cardNumberChanger({ number: card.stackingOrder, position: card.position })
+                const tarotCardInfo = tarotInfomation({ infoType: "card", cardNumber: card.stackingOrder })
+                return (
+                    // <div key={index}>
+                    //     <div>stackingOrder: {card.stackingOrder}</div>
+                    //     <div>position: {card.position}</div>
+                    //     <div>{tarotCard.number} {tarotCard.name}の{tarotCard.position}</div>
+                    // </div>
+                    <Box key={index} ml={2} mb={1}>
+                        <Text m={0}>
+                            {tarotCard.number} {tarotCard.name}の{tarotCard.position}
+                        </Text>
+                        <Text my={0} ml={2}>
+                            {tarotCardInfo}
+                        </Text>
+                    </Box>
+                )
+            })}
         </>
     )
 }
