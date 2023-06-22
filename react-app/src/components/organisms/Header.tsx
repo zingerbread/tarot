ea18e476b5ad124c9d97d19cb2b3f5d736e118ab
Tanahashi Shun <tanahashi@hashup.co.jp>
2023-06-22 20:57:33 +0900

    ヘッダーのテキストを変更

--- react-app/src/components/organisms/Header.tsx
+++ react-app/src/components/organisms/Header.tsx
@@ -1,13 +1,13 @@
 import { Box, Heading } from '@chakra-ui/react'
 
 const Header = () => {
-  return (
-    <Box bg="tomato" w="100%" p={4} color="white">
-      <Heading as="h1" size="xl">
-        Chakra UI
-      </Heading>
-    </Box>
-  )
+    return (
+        <Box bg="tomato" w="100%" p={4} color="white">
+            <Heading as="h1" size="xl">
+                Tarot Cards
+            </Heading>
+        </Box>
+    )
 }
 
 export default Header
