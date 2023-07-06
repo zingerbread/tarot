feca10423ae92196645a047575e403a214a7d088
Tanahashi Shun <tanahashi@hashup.co.jp>
2023-07-06 18:07:56 +0900

    バージョン表記の追加

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,13 +1,18 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Header = () => {
-    return (
-        <Box bg="tomato" w="100%" p={4} color="white">
-            <Heading as="h1" size="xl">
-                Tarot Cards
-            </Heading>
-        </Box>
-    )
-}
+  return (
+    <Box bg="tomato" w="100%" p={4} color="white">
+      <Heading as="h1" size="xl">
+        Tarot Cards
+      </Heading>
+      <Box ml="16" mt="-8">
+        <Text fontSize="md" as="b">
+          version 0.1.0
+        </Text>
+      </Box>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
