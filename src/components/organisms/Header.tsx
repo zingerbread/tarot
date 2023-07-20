cf45fb284f801d2a7534cd0287269fad63517a07
Tanahashi Shun <tanahashi@hashup.co.jp>
2023-07-21 06:08:40 +0900

    横幅を修正

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Box bg="tomato" w="100%" p={4} color="white">
+    <Box bg="tomato" p={4} color="white">
       <Heading as="h1" size="xl">
         Tarot Cards
       </Heading>
